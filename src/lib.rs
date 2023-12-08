608ef9b71be486398bfca1662926fdc80a7d2901
Yiran Sheng <shengyiran@gmail.com>
2023-12-08 22:02:47 +0000

    simplify the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,44 @@
-use parking_lot::RwLock;
+use parking_lot::{Mutex, MutexGuard};
 use socket2::{Domain, Protocol, Socket, Type};
 use std::net::{SocketAddr, SocketAddrV4, UdpSocket};
-use std::sync::Arc;
 use tun_tap::Iface;
 
-use std::{eprintln, io};
+use std::io;
+
+pub struct Peer {
+    endpoint: Mutex<Option<SocketAddrV4>>,
+}
 
 pub struct Device {
-    use_connected_socket: bool,
-    listen_port: u16,
+    udp: UdpSocket,
     iface: Iface,
     peer: Peer,
 }
 
-pub struct Peer {
-    endpoint: RwLock<Endpoint>,
-}
-
-#[derive(Default)]
-pub struct Endpoint {
-    pub addr: Option<SocketAddrV4>,
-    pub conn: Option<Arc<UdpSocket>>,
-}
-
 impl Device {
-    pub fn new(use_connected_socket: bool, listen_port: u16, iface: Iface, peer: Peer) -> Self {
+    pub fn new(iface: Iface, peer: Option<SocketAddrV4>) -> Self {
+        let udp = new_udp_socket(19988).unwrap();
         Self {
-            use_connected_socket,
-            listen_port,
+            udp,
             iface,
-            peer,
+            peer: Peer {
+                endpoint: Mutex::new(peer),
+            },
         }
     }
 
-    pub fn start(self, udp: Option<Arc<UdpSocket>>) -> io::Result<()> {
-        let udp = if let Some(sock) = udp {
-            sock
-        } else {
-            Arc::new(new_udp_socket(self.listen_port)?)
-        };
-
-        let dev1 = Arc::new(self);
-        let dev2 = Arc::clone(&dev1);
-        let jh1 = std::thread::spawn(move || {
-            if let Err(err) = dev1.loop_listen_iface() {
-                eprintln!("err loop 1: {:?}", err);
-            }
-        });
-        let jh2 = std::thread::spawn(move || {
-            if let Err(err) = dev2.loop_listen_udp(udp) {
-                eprintln!("err loop 2: {:?}", err);
-            }
-        });
-
-        jh1.join().unwrap();
-        jh2.join().unwrap();
-
-        Ok(())
-    }
-
-    fn loop_listen_iface(self: Arc<Self>) -> io::Result<()> {
+    pub fn loop_listen_iface(&self) -> io::Result<()> {
         let mut buf = [0u8; 1504];
         {
-            let endpoint = self.peer.endpoint.read();
-            if let Some(ref conn) = endpoint.conn {
-                eprintln!("initiating peer connection..");
-                conn.send("hello?".as_bytes())?;
+            let peer = self.peer.endpoint();
+            if let Some(peer_addr) = peer.as_ref() {
+                eprintln!("initiating \"handshake\" to peer: {peer_addr}");
+                self.udp.send_to("hello?".as_bytes(), peer_addr)?;
             }
         }
 
         loop {
             let nbytes = self.iface.recv(&mut buf[..])?;
-            let endpoint = self.peer.endpoint.read();
-
             match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
                 Ok(iph) => {
                     let src = iph.source_addr();
@@ -81,20 +47,20 @@ impl Device {
                 }
                 _ => {}
             }
-
-            if let Some(ref conn) = endpoint.conn {
-                conn.send(&buf[..nbytes])?;
+            let peer = self.peer.endpoint();
+            if let Some(peer_addr) = peer.as_ref() {
+                self.udp.send_to(&buf[..nbytes], peer_addr)?;
             } else {
                 eprintln!("..no peer");
             }
         }
     }
 
-    fn loop_listen_udp(self: Arc<Self>, sock: Arc<UdpSocket>) -> io::Result<()> {
+    pub fn loop_listen_udp(&self) -> io::Result<()> {
         let mut buf = [0u8; 1504];
 
         loop {
-            let (nbytes, peer_addr) = sock.recv_from(&mut buf[..])?;
+            let (nbytes, peer_addr) = self.udp.recv_from(&mut buf[..])?;
             eprintln!("Got packet of size: {nbytes}, from {peer_addr}");
 
             match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
@@ -108,24 +74,11 @@ impl Device {
                 }
             }
 
-            if let SocketAddr::V4(peer_addr) = peer_addr {
-                self.peer.set_endpoint(peer_addr);
-
-                if self.use_connected_socket && !self.peer.is_connected() {
-                    if &buf[..nbytes] == b"hello?" {
-                        eprintln!("Connecting peer: {peer_addr}");
-                        let sock = self.peer.connect_endpoint(self.listen_port)?;
-
-                        let dev = Arc::clone(&self);
-                        let _ = std::thread::spawn(move || {
-                            if let Err(err) = dev.loop_listen_udp(sock) {
-                                eprintln!("err loop 2 (2): {:?}", err);
-                            }
-                        });
-                        continue;
-                    }
+            if let SocketAddr::V4(peer_addr_v4) = peer_addr {
+                if &buf[..nbytes] == b"hello?" {
+                    self.peer.set_endpoint(peer_addr_v4);
+                    continue;
                 }
-
                 self.iface.send(&buf[..nbytes])?;
             }
         }
@@ -145,36 +98,14 @@ fn new_udp_socket(port: u16) -> io::Result<UdpSocket> {
 }
 
 impl Peer {
-    pub fn new(endpoint: Endpoint) -> Self {
-        Self {
-            endpoint: RwLock::new(endpoint),
+    fn endpoint(&self) -> MutexGuard<Option<SocketAddrV4>> {
+        self.endpoint.lock()
+    }
+
+    fn set_endpoint(&self, addr: SocketAddrV4) {
+        let mut endpoint = self.endpoint.lock();
+        if endpoint.is_none() {
+            *endpoint = Some(addr);
         }
-    }
-
-    pub fn set_endpoint(&self, addr: SocketAddrV4) {
-        let mut endpoint = self.endpoint.write();
-        if endpoint.addr != Some(addr) {
-            *endpoint = Endpoint {
-                addr: Some(addr),
-                conn: None,
-            }
-        };
-    }
-
-    pub fn is_connected(&self) -> bool {
-        let endpoint = self.endpoint.read();
-        endpoint.conn.is_some()
-    }
-
-    pub fn connect_endpoint(&self, port: u16) -> io::Result<Arc<UdpSocket>> {
-        let mut endpoint = self.endpoint.write();
-
-        let conn = new_udp_socket(port)?;
-        conn.connect(endpoint.addr.unwrap())?;
-        let conn = Arc::new(conn);
-
-        endpoint.conn = Some(Arc::clone(&conn));
-
-        Ok(conn)
     }
 }
--- src/wontun.rs
+++ src/wontun.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use std::{eprintln, io, net::SocketAddr};
-use wontun::{Device, Endpoint, Peer};
+use std::{eprintln, io, net::SocketAddr, sync::Arc};
+use wontun::Device;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -12,22 +12,35 @@ struct Cli {
 fn run(peer_addr: Option<&str>) -> io::Result<()> {
     let iface = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
 
-    let use_connected_socket = peer_addr.is_none();
-    let peer = Peer::new(Endpoint::default());
-
-    let sock = peer_addr
+    let peer = peer_addr
         .and_then(|addr| addr.parse::<SocketAddr>().ok())
         .and_then(|addr| {
             if let SocketAddr::V4(addr) = addr {
-                eprintln!("Peer: {addr}");
-                peer.set_endpoint(addr);
-                peer.connect_endpoint(19988).ok()
+                Some(addr)
             } else {
                 None
             }
         });
 
-    Device::new(use_connected_socket, 19988, iface, peer).start(sock)
+    let dev = Device::new(iface, peer);
+    let dev1 = Arc::new(dev);
+    let dev2 = Arc::clone(&dev1);
+
+    let jh1 = std::thread::spawn(move || {
+        if let Err(err) = dev1.loop_listen_iface() {
+            eprintln!("err loop 1: {:?}", err);
+        }
+    });
+    let jh2 = std::thread::spawn(move || {
+        if let Err(err) = dev2.loop_listen_udp() {
+            eprintln!("err loop 2: {:?}", err);
+        }
+    });
+
+    jh1.join().unwrap();
+    jh2.join().unwrap();
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
